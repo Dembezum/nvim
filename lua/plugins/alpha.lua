ad6ca44bbd0360d8ec8b9afd32393365b22a9456
Dembezum <dembezuuma@gmail.com>
2024-02-07 11:52:27 +0100

    Update alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,26 +1,23 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
--- Set new header
+-- Set header
 dashboard.section.header.val = {
-    " ________                      __  __                     ",
-    "/\\_____  \\                    /\\ \\/\\ \\  __                ",
-    "\\/____//'/'  __  __    ___ ___\\ \\ \\ \\ \\/\\_\\    ___ ___    ",
-    "     //'/'  /\\ \\/\\ \\ /' __` __`\\ \\ \\ \\ \\/\\ \\ /' __` __`\\  ",
-    "    //'/'___\\ \\ \\_\\ \\/\\ \\/\\ \\/\\ \\ \\ \\_/ \\ \\ \\/\\ \\/\\ \\/\\ \\ ",
-    "    /\\_______\\ \\____/\\ \\_\\ \\_\\ \\_\\ `\\___/\\ \\_\\ \\_\\ \\_\\ \\_\\",
-    "    \\/_______/\\/___/  \\/_/\\/_/\\/_/`\\/__/  \\/_/\\/_/\\/_/\\/_/",
-    "                                                          ",
-    "                                                          "
+    "    _   __                _         ",
+    "   / | / /__  ____ _   __(_)___ ___ ",
+    "  /  |/ / _ \\/ __ \\ | / / / __ `__ \\",
+    " / /|  /  __/ /_/ / |/ / / / / / / /",
+    "/_/ |_/\\___/\\____/|___/_/_/ /_/ /_/ ",
+    "                                    ",
 }
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
-    dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-    dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button("q", "  > Quit NVIM", ":qa<CR>")
+    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
 }
 
 -- Set footer (commented out since it's related to a fork and pull request)
